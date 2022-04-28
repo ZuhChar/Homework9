c9d8c92f795b1ce1cedb84972e3b3fc4f6094fd5
ZuhChar <zcrimmel@uwyo.edu>
2022-04-27 19:37:43 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -86,7 +86,7 @@ int main()
         int ambigOutUse = 0;
         int ambigInUse = 0;
         int eol = 0;
-        char *arguments[10] = {"ls", "-l", NULL};
+        char *arguments[10] = {lexeme, "-l", NULL};
         // current = calloc(1, sizeof(Node));
         while (rtn != EOL)
         {
