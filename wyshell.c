d24cf0bc2cf2d501c949ced632dc2cbeecb948e2
ZuhChar <zcrimmel@uwyo.edu>
2022-04-27 21:16:18 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -191,7 +191,7 @@ int main()
                 break;
             }
             if(amp == 0){
-                wait(amp);
+                wait(*amp);
             }
         }
         // Create a child to run the command in
