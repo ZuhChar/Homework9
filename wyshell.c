14f6d4a0c4363d9fe8236810f6d0b56837f34543
ZuhChar <zcrimmel@uwyo.edu>
2022-04-27 20:58:43 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -204,7 +204,7 @@ int main()
                 {
                     printf("Terminated Incorrectly\n");
                 }
-                if (amp == 0)
+                if (amp == 1)
                 {
                     return 0;
                 }
